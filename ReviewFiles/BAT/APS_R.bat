953e5b492c235a833ce52b7bb8dcbd8e0ff1cdb8
shivangKheradiya <shivangatul@gmail.com>
2024-07-17 00:17:06 +0530

    BASIC FOLDER STRUCTURE

---
+++ ReviewFiles/BAT/APS_R.bat
@@ -0,0 +1,3 @@
+:: PDMS Batch file location--------PDMS-module-Projetname user-pass MDB Review macro 
+"C:\Program Files (x86)\AVEVA\Everything3D2.10\launch.bat" DESIGN APS SYSTEM/XXXXXX /ALL $m/S:\REVIEW\REVIEW_M\APS-All.txt 
+
